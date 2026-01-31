7e3b3f7f38f365b39c483d317f9d46deb48dee26
Jay Ariwala <152592458+jay091105@users.noreply.github.com>
2026-01-31 16:29:14 +0530

    Delete backend directory

--- backend/src/types/multer.d.ts
+++
@@ -1,5 +0,0 @@
-import { AuthRequest } from '../middleware/auth';
-
-export interface MulterAuthRequest extends AuthRequest {
-  files?: Express.Multer.File[] | { [fieldname: string]: Express.Multer.File[] };
-}
